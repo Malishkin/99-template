7a10b906c8cff4cfc77910f46d34c44f32fe6181
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-03 20:01:17 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import "./App.css";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
 
+const texts = [
+  "Click me 1",
+  "Click me 2",
+  "Click me 3",
+  "Click me 4",
+  "Click me 5",
+  "Click me 6",
+  "Click me 7",
+];
 function App() {
   const [count, setCount] = useState(0);
   const incrementCount = () => setCount(count + 1);
   return (
     <div className="App">
       <Counter count={count} />
-      <Button onClick={incrementCount} />
-      <Button onClick={incrementCount} />
-      <Button onClick={incrementCount} />
-      <Button onClick={incrementCount} />
+      {texts.map((text, index) => (
+        <Button key={index} onClick={incrementCount} text={text} />
+      ))}
     </div>
   );
 }
