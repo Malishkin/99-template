16a43017f233275a22db042cbaf1cb9ba33c0651
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-04 11:01:40 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Card from "./components/Card";
 
-function App() {
-  return <div className="App"></div>;
+function App ()
+{
+  const cardData = [
+    { title: "Card 1", description: "This is card 1" },                                      
+    { title: "Card 2", description: "This is card 2" },                                                                         
+    { title: "Card 3", description: "This is card 3" },
+    ]
+  return <div className="App">
+    {cardData.map((card, index) => (
+      <Card key={index} title={card.title} description={card.description} />
+    ))}
+  </div>;
 }
 
 export default App;
