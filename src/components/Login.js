ef8ff2a3b1df3a61f08b07dae89d8237f31155e4
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-04 15:44:40 +0300

    .

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
+    console.log(userData);
+    alert(JSON.stringify(userData));
+  }
+  return (
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+export default Login;
