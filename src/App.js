2eaa03d80ec2b0db37a783a2f87d0340be837604
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-04 18:55:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Wrapper from "./components/Wrapper";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Wrapper color="lightblue">
+        <h2>Inside of the Wrapper</h2>
+        <button>Click me!</button>
+      </Wrapper>
+      <Wrapper color="lightgreen">
+        <p>Some description</p>
+        <input type="text" placeholder="Some value" />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
