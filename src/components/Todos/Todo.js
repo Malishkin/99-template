7973b2d06c7bc7eb4a001223f16126801066a660
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-10 17:09:14 +0300

    .

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -2,11 +2,8 @@ import styles from "./Todo.module.css";
 
 function Todo({ todo, index, deleteTodo }) {
   return (
-    <div className={styles.todo}>
+    <div onDoubleClick={() => deleteTodo(index)} className={styles.todo}>
       <div className={styles.todoText}>{todo}</div>
-      <button className={styles.todoIcon} onClick={() => deleteTodo(index)}>
-        Delete
-      </button>
     </div>
   );
 }
