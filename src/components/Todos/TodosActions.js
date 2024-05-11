5e77ca32db0583f6f8b77a949d1eefced13f96e0
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-11 12:47:49 +0300

    .

--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -1,9 +1,9 @@
 function TodosActions() {
   return (
-    <div>
+    <>
       <button>Add Todo</button>
       <button>Remove Todo</button>
-    </div>
+    </>
   );
 }
 export default TodosActions;
