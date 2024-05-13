38711942eacab23d241c75939db345f9d3acddcd
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-13 10:50:22 +0300

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
+          <Route path="*" element={<h1>Not Found</h1>} />
           <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="contacts" element={<h1>Contacts</h1>} />
         </Routes>
       </div>
     </BrowserRouter>
