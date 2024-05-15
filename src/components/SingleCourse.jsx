9762efc5acb1a656552d21d4879ed4afb87f4a1e
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-15 10:46:46 +0300

    .

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
 
 const SingleCourse = () => {
-  return <h1>Single Course Info</h1>;
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const course = courses.find((course) => course.slug === params.courseSlug);
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course, navigate]);
+
+  return (
+    <>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
+      <Link to=".." relative="path">
+        All courses
+      </Link>
+    </>
+  );
 };
 
 export default SingleCourse;
