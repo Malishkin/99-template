43721288230784a7502d5c0622ac351776fd5194
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-15 11:19:06 +0300

    .

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,15 +1,43 @@
 import courses from "../data/courses";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { useState, useEffect } from "react";
+const SORT_KEYS = ["title", "slug", "id"];
+function sortKeys(courses, key) {
+  const sortedCourses = [...courses];
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortedCourses;
+  }
+  sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+  return sortedCourses;
+}
 const Courses = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { sort } = queryString.parse(location.search);
+  const [sortKey, setSortKey] = useState(sort);
+  const [sortedCourses, setSortedCourses] = useState(
+    sortKeys(courses, sortKey)
+  );
+  useEffect(() => {
+    setSortKey(sort);
+  }, [sort]);
+
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate(".");
+      setSortKey();
+      setSortedCourses(courses);
+    }
+  }, [sortKey, navigate]);
+
   return (
     <>
-      <h1>Courses</h1>
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : "Courses"}</h1>
 
-      {courses.map((course) => (
+      {sortedCourses.map((course) => (
         <div key={course.id}>
-          <Link to={course.slug} className="courseLink">
-            {course.title}
-          </Link>
+          <Link to={course.slug}>{course.title}</Link>
         </div>
       ))}
     </>
